32dc94294c2a1d652d287607aadb54bdf51a2175
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-07-16 00:06:30 +0200

    fix mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,24 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.menu-open-btn'),
+//     closeMenuBtn: document.querySelector('.menu-close-btn'),
+//     menu: document.querySelector('.mob-menu'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//   }
+// })();
+
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
     menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -10,5 +26,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
