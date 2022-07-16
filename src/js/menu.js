a4677833a006de9d5e9774d3f735c5fd0129cadb
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-07-16 20:24:03 +0200

    update container, add animate.css, add aos

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('.menu-open-btn'),
-//     closeMenuBtn: document.querySelector('.menu-close-btn'),
-//     menu: document.querySelector('.mob-menu'),
-//   };
-
-//   refs.openMenuBtn.addEventListener('click', toggleMenu);
-//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-//   function toggleMenu() {
-//     refs.menu.classList.toggle('is-hidden');
-//   }
-// })();
-
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('.menu-open-btn'),
