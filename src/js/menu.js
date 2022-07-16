fa6665e594edc64ca9f2e8c5ce55e310f2c69024
Nelia Bochenkova <nelya.teslenko.95@gmail.com>
2022-07-16 11:36:55 +0300

    add change

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
     menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
