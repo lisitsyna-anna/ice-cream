99e445234908d22a18d50836c3ea6b92e8c8205b
lisitsyna-anna <104447922+lisitsyna-anna@users.noreply.github.com>
2022-07-17 10:15:45 +0000

    Deploying to gh-pages from @ lisitsyna-anna/dream-team-project@6da7844aad305a64fcf4b3ff5d5faf99db44a4ad 🚀

--- index.90bcab8e.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[modal-about-open]"),closeModalBtn:document.querySelector("[modal-about-close]"),modal:document.querySelector("[modal-about]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.90bcab8e.js.map
