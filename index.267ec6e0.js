d87882c927210d307dd8e48eafb12cf4d387468a
lisitsyna-anna <104447922+lisitsyna-anna@users.noreply.github.com>
2022-07-20 11:38:03 +0000

    Deploying to gh-pages from @ lisitsyna-anna/dream-team-project@f4f86311a28ea716eab4e6f1fc6c3aadefaef217 🚀

--- index.267ec6e0.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[loc-modal-open]"),closeModalBtn:document.querySelector("[loc-modal-close]"),modal:document.querySelector("[loc-modal]")};function e(){document.body.classList.toggle("no-scroll"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.267ec6e0.js.map
---
+++ index.cab70498.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-loc-modal-open]"),closeModalBtn:document.querySelector("[data-loc-modal-close]"),modal:document.querySelector("[data-loc-modal]")};function e(){document.body.classList.toggle("no-scroll"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.cab70498.js.map
